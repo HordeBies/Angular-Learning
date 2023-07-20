5bb138c0383fdd9112a60f8ce7d332c3bbc5f356
Mehmet Demirci <oa.mehmetdmrc@gmail.com>
2023-07-20 12:08:55 +0300

    Added project for module system

---
+++ Comps/src/app/elements/elements-home/elements-home.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ElementsHomeComponent } from './elements-home.component';
+
+describe('ElementsHomeComponent', () => {
+  let component: ElementsHomeComponent;
+  let fixture: ComponentFixture<ElementsHomeComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [ElementsHomeComponent]
+    });
+    fixture = TestBed.createComponent(ElementsHomeComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
